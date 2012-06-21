cc89ada7ee631b6fbeb332fa4f56974fd4ad5298
Arnault <arnault@lal.in2p3.fr>
2012-06-21 11:49:12 +0200

    Fully dynamic setting of CMTROOT

--- do.bat
+++ do.bat
@@ -1,9 +1,9 @@
 
 cd %CMTROOT%
-del /F/S/Q test
-mkdir test
+rem del /F/S/Q test
+rem mkdir test
 cd test
-python ..\generator.py projects=3 packages=8
+rem python ..\generator.py projects=3 packages=8
 dot -Tjpg generator.dot >generator.jpg
 generator.jpg @
 cd build && cmake --build=. ..\CMakeLists.txt && cd ..
--- setup.bat
+++ setup.bat
@@ -1,5 +1,6 @@
 
-set CMTROOT=C:\Arnault\CMTCMake\CMT
+for /f "delims=" %%i in ('cd') do Set pwd=%%i
+set CMTROOT=%pwd%
 
 set PATH=c:\Install\python27;c:\Install\Graphviz\bin;c:\Install\CMake\bin;%PATH%
 set PYTHONPATH=c:\Install\Waf;%PYTHONPATH%
