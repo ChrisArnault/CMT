cf9767007c96e0cc2e33da552852aa7e6d194477
unknown <ARNAULT@nb-arnault3.lal.in2p3.fr>
2012-06-25 15:24:20 +0200

    Add test management (show) Cleanup setup.bat add DartConfigure.tcl to ignore

--- setup.bat
+++ setup.bat
@@ -1,9 +1,15 @@
+rem >>>> configure git???
+set path=\install\git\bin;%path% 
 
+rem >>>> discover where we are
 for /f "delims=" %%i in ('cd') do Set pwd=%%i
 set CMTROOT=%pwd%
 
+rem >>>> configure various external sofware
+rem >>>>   python
+rem >>>>   graphiz
+rem >>>>   CMake
 set PATH=c:\Install\python27;c:\Install\Graphviz\bin;c:\Install\CMake\bin;%PATH%
-set PYTHONPATH=c:\Install\Waf;%PYTHONPATH%
-set PATH=c:\Install\Waf;%PATH%
+
 
 
