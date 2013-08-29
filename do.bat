abe176fe99b386d52aafcbb226aab7cc3c33b547
Christian Arnault <arnault@lal.in2p3.fr>
2013-08-29 09:02:00 +0200

    Initial cleanup for the new HWAF branch

--- do.bat
+++ do.bat
@@ -1,13 +1,9 @@
 
-cd %CMTROOT%
-rem del /F/S/Q test
-rem mkdir test
+rmdir /S/Q test
+mkdir test
 cd test
-rem python ..\generator.py projects=3 packages=8
+python ..\generator.py projects=3 packages=8
 dot -Tjpg generator.dot >generator.jpg
-rem generator.jpg @
-mkdir build
-cd build && cmake --build=. ..\CMakeLists.txt && cd ..
 cd ..
-rem test\work.sln
+
 
--- setup.bat
+++ setup.bat
@@ -1,15 +1,8 @@
-rem >>>> configure git???
-set path=\install\git\bin;%path% 
 
 rem >>>> discover where we are
 for /f "delims=" %%i in ('cd') do Set pwd=%%i
 set CMTROOT=%pwd%
 
-rem >>>> configure various external sofware
-rem >>>>   python
-rem >>>>   graphiz
-rem >>>>   CMake
-set PATH=c:\Install\python27;c:\Install\Graphviz\bin;c:\Install\CMake\bin;%PATH%
 
 
 
